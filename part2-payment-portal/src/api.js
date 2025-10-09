2b086c2a13acf7580ed5cca838ae46f4d3c69d45
ST10268411 <126479662+ST10268411@users.noreply.github.com>
2025-10-09 08:25:58 +0200

    Delete part2-payment-portal directory

--- part2-payment-portal/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: '/api',        // Vite proxy forwards this to http://localhost:5000
-  withCredentials: true,  
-  headers: { 'Content-Type': 'application/json' }
-});
-
-export default api;
